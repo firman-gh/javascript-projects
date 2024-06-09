6fb9a1126bcfac6b619be58261cd24c080f81c66
firman-gh <firmansyahirsyaad76@gmail.com>
2024-06-09 12:05:59 +0700

    latihan nodejs web server

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,29 +1,38 @@
+// membuat HTTP server
 const http = require('http');
-
+// membuat fungsi Request listener dengan parameter request dan response
 const requestListener = (request, response) => {
+    // membuat response header
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('Powered-By', 'Node.js');
-
+    // membuat properti method untuk routing request 
     const {method, url} = request;
-
+    // logika routing request dan response pada client
     if(url === '/') {
         if(method === 'GET') {
+            // membuat respon status menggunakan response.statusCode
             response.statusCode = 200;
+            // membuat respon body menggunakan response.end
             response.end(JSON.stringify({
                 message: 'Ini adalah homepage',
             }));
         } else {
+            // membuat respon status menggunakan response.statusCode
             response.statusCode = 400;
+            // membuat respon body menggunakan response.end
             response.end(JSON.stringify({
                 message: `Halaman tidak dapat diakses dengan ${method} request`,
             }));
         }
     } else if(url === '/about') {
         if(method === 'GET') {
+            // membuat respon status menggunakan response.statusCode
             response.statusCode = 200;
+            // membuat respon body menggunakan response.end
             response.end(JSON.stringify({
                 message: 'Halo! Ini adalah halaman about',
             }));
+          // membuat body request
         } else if(method === 'POST') {
                 let body = [];
         
@@ -34,27 +43,33 @@ const requestListener = (request, response) => {
                 request.on('end', () => {
                     body = Buffer.concat(body).toString();
                     const { name } = JSON.parse(body);
+                    // membuat respon status menggunakan response.statusCode
                     response.statusCode = 200;
+                    // membuat respon body menggunakan response.end
                     response.end(JSON.stringify({
                         message: `Halo, ${name}! Ini adalah halaman about`,
                     }));
                 });
         } else {
+            // membuat respon status menggunakan response.statusCode
             response.statusCode = 400;
+            // membuat respon body menggunakan response.end
             response.end(JSON.stringify({
                 message: `Halaman tidak dapat diakses dengan ${method} request`
             }));
         }
     } else {
+        // membuat respon status menggunakan response.statusCode
         response.statusCode = 404;
+        // membuat respon body menggunakan response.end
         response.end(JSON.stringify({
             message: 'Halaman tidak ditemukan',
         }))
     }
 };
-
+// membuat Request listener
 const server = http.createServer(requestListener);
-
+// membuat method listen yang diberi nilai port, host, dan listening listener
 const port = 5000;
 const host = 'localhost';
 
